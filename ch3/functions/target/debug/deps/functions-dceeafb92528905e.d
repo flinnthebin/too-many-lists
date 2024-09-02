c36cc77f68244051279c7c4a20df4fb9a180a3cf
Chris Flinn <christopher.flinn@gmail.com>
2024-09-02 18:04:40 +1000

    3-1

---
+++ ch3/functions/target/debug/deps/functions-dceeafb92528905e.d
@@ -0,0 +1,5 @@
+/home/archer/git/rust-book/ch3/functions/target/debug/deps/functions-dceeafb92528905e: src/main.rs
+
+/home/archer/git/rust-book/ch3/functions/target/debug/deps/functions-dceeafb92528905e.d: src/main.rs
+
+src/main.rs:
---
+++ ch3/functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/archer/git/rust-book/ch3/functions/target/debug/functions: /home/archer/git/rust-book/ch3/functions/src/main.rs
