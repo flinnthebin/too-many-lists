080b1a611845d85c815a779acf1105933eaa02ee
Chris Flinn <christopher.flinn@gmail.com>
2024-09-02 18:37:52 +1000

    expression

--- ch3/functions/src/main.rs
+++ ch3/functions/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    another_function(5)
-}
+    let y = {
+        let x = 3;
+        x + 1
+    };
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+    println!("The value of y is {y}")
 }
