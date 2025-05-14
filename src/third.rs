d976ab41ce104fd17d1816dbd9301c992a8cd886
chris flinn <christopher.flinn@gmail.com>
2025-05-14 19:54:10 +1000

    iter

--- src/third.rs
+++ src/third.rs
@@ -11,6 +11,10 @@ struct Node<T> {
     next: Link<T>,
 }
 
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
@@ -34,6 +38,23 @@ impl<T> List<T> {
     pub fn head(&self) -> Option<&T> {
         self.head.as_ref().map(|node| &node.elem)
     }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
 }
 
 #[cfg(test)]
@@ -53,5 +74,13 @@ mod test {
         assert_eq!(list.head(), None);
         let list = list.tail();
         assert_eq!(list.head(), None);
+    }
+    #[test]
+    fn iter() {
+        let list = List::new().prepend(1).prepend(2).prepend(3);
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 }
